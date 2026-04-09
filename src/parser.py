86f0ed7df7c7ea662021f3fb325163409da22e3b
Clifford <nwannachumaclifford@gmail.com>
2026-04-09 11:03:47 +0100

    cleanup: remove deprecated wrapper files (parser.py, matcher.py)

--- src/parser.py
+++
@@ -1,6 +0,0 @@
-# DEPRECATED: Use parser_v2 directly
-# This file is kept for backward compatibility but will be removed in v3.0
-# Import from parser_v2 instead: from src.parser_v2 import EnhancedCVParser
-from .parser_v2 import DynamicCVParser, EnhancedCVParser
-
-__all__ = ['DynamicCVParser', 'EnhancedCVParser']
